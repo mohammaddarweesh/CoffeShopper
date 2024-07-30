cf1106b731a22c08f4da15e294ecbd6f8820caef
Mohammad Darweesh <mhamaddarweesh0@gmail.com>
2024-07-30 22:50:47 +0300

    prepare context and dataseeders

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,31 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly.GetName().Name;
+var defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AspNetIdentityDbContext>(options => 
+    options.UseSqlServer(defaultConnectionString, b => b.MigrationsAssembly(assembly)));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AspNetIdentityDbContext>();
+
+builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<IdentityUser>()
+    .AddConfigurationStore(options =>
+    {
+        options.ConfigureDbContext = b => b.UseSqlServer(defaultConnectionString, opt => opt.MigrationsAssembly(assembly));
+    })
+    .AddOperationalStore(options => {
+        options.ConfigureDbContext = b => b.UseSqlServer(defaultConnectionString, opt => opt.MigrationsAssembly(assembly));
+    })
+    .AddDeveloperSigningCredential();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseIdentityServer();
 
 app.Run();
