ce908c94e4f775bfd4ddeee73eaf5735521a1091
Mohammad Darweesh <mhamaddarweesh0@gmail.com>
2024-08-06 21:48:18 +0300

    add client application(blaxor app) and get coffe shops from it

---
+++ Client/Pages/CoffeShops.razor.cs
@@ -0,0 +1,26 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Client.Pages
+{
+    public partial class CoffeShops
+    {
+        private List<CoffeShopModel> Shops = new();
+        [Inject] private HttpClient HttpClient { get; set; }
+        [Inject] private IConfiguration config { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var result = await HttpClient.GetAsync(config["apiUrl"] + "/api/CoffeShop");
+
+            if (result.IsSuccessStatusCode)
+            {
+                Shops = await result.Content.ReadFromJsonAsync<List<CoffeShopModel>>();
+            }
+        }
+
+    }
+}
